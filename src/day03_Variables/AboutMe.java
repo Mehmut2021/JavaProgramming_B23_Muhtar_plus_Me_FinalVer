c88143922bde2ad181ea117adf61de79bc81f436
MM-Uyghur <mm.well@outlook.com>
2021-09-24 21:35:36 -0400

    initial

--- src/day03_Variables/AboutMe.java
+++ src/day03_Variables/AboutMe.java
@@ -2,6 +2,7 @@ package day03_Variables;
 
 public class AboutMe {
 
+
     public static void main(String[] args) {
         System.out.println("\tHello everyone, My name is Matthew\nmy favorite music is \"Rock from 80's\"\nMy favorite book is \"The Wheel of Time\"");
 
